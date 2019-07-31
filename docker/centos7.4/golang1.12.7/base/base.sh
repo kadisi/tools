1290069efb581e4bb8701a173404721922e5aa2a
kadisi <iamkadisi@163.com>
2019-07-31 14:35:29 +0800

    golang 1.12.7
    Signed-off-by: kadisi <iamkadisi@163.com>

---
+++ docker/centos7.4/golang1.12.7/base/base.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# #############################################
+# Copyright (c) 2019-2039 All rights reserved.
+# #############################################
+#
+# Name:  build.sh
+# Date:  2019-02-13 11:29
+# Author:   zhangjie
+# Email:   iamzhangjie0619@163.com
+# Desc:  
+#
+#
+
+docker build -t jiulongzaitian/centos-7.4.1708-go-1.12.7:base --force-rm .
+
+docker push jiulongzaitian/centos-7.4.1708-go-1.12.7:base
+
